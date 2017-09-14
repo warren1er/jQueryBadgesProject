47b9a748dba0517de992a823e4220ea5d6f98fb0
warren1er <kbousardo@gmail.com>
2017-09-14 15:20:02 +0200

    completed

--- src/assets/main.js
+++ src/assets/main.js
@@ -3,8 +3,36 @@ $(function() {
   $.ajax({
     url:'https://www.codeschool.com/users/shendoo.json', dataType: 'jsonp',
     success: function(response){
-      
+      addCourses(response.courses.completed);
     }
-  })
+  });
 
+  function addCourses(courses){
+
+    var $badges = $('#badges');
+
+    courses.forEach(function(course){
+
+      console.log(course);
+
+    var $course =  $('<div />',{
+        'class': 'course'
+      }).appendTo($badges);
+
+      $('<h3 />',{
+        text: course.title
+      }).appendTo($badges);
+
+      $('<img />',{
+        src: course.badge
+      }).appendTo($badges);
+
+      $('<a />',{
+        href: url,
+        target: _blank,
+        btn: btn btn-primary,
+        text: See Course
+      }).appendTo($badges);
+    })
+  }
 });
